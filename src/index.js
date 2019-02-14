314ce0b8383f4d3c369e38a6ec9e31b6dbf2f789
Joey Saha <joeysaha@hotmail.com>
2019-02-13 21:46:34 -0500

    declarable winner, no replacing moves, has ending

--- src/index.js
+++ src/index.js
@@ -44,6 +44,9 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
@@ -61,7 +64,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}.`;
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner winner chicken dinner: ' + winner;
+    } else {
+      status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}.`;
+    }
 
     return (
       <div>
@@ -120,4 +129,12 @@ function calculateWinner(squares) {
     [0, 4, 8],
     [2, 4, 6]
   ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] &&
+      squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
